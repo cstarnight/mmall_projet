05677dfb3da5cdda4dd51eb8e2a2b92edf4f36ab
cstarnight <cstarnight@163.com>
2017-12-08 17:11:58 +0800

    v1.0 end

--- src/main/java/com/mmall/dao/CartMapper.java
+++ src/main/java/com/mmall/dao/CartMapper.java
@@ -1,6 +1,10 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Cart;
+import com.mmall.pojo.Category;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface CartMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +18,19 @@ public interface CartMapper {
     int updateByPrimaryKeySelective(Cart record);
 
     int updateByPrimaryKey(Cart record);
+
+    Cart selectCartByUserIdProductId(@Param("userId") Integer userId, @Param("productId") Integer productId);
+
+    List<Cart> selectCartByUserId(Integer userId);
+
+    int selectCartProductCheckedStatusByUserId(Integer userID);
+
+    int deleteByUserIdAndProductIds(@Param("userId") Integer userId, @Param("productIdList") List<String> productIdList);
+
+    int checkedOrUnCheckedProduct(@Param("userId") Integer userId,@Param("productId") Integer productId,@Param("checked") Integer checked);
+
+    int selectCartProductCount(Integer userId);
+
+    List<Cart> selectCheckedCartByUserId(Integer userId);
+
 }
